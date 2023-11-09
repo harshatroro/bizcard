7811e79e12b6e44f55365ced880663280474a4e1
Harsh Srivastava <sharsh2357@bbdnitm.ac.in>
2023-11-09 20:31:44 +0530

    Create Repository

---
+++ lib/repository/repository.dart
@@ -0,0 +1,16 @@
+import 'package:bizcard/db/database.dart';
+import 'package:bizcard/models/contact.dart';
+
+class Repository {
+  final Database database;
+
+  Repository({
+    required this.database,
+  });
+
+  Future<List<Contact>> getContacts() async {
+    final contactsJson = await database.queryAllRows();
+    final contacts = contactsJson.map((json) => Contact.fromJson(json)).toList();
+    return contacts;
+  }
+}
